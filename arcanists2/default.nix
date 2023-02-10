688ac1ceb872ceb8970932f0857b3c146013dc2a
qwbarch <qwbarch@gmail.com>
2023-02-09 20:11:35 -0500

    Add install phase

--- arcanists2/default.nix
+++ arcanists2/default.nix
@@ -8,4 +8,11 @@ stdenv.mkDerivation rec {
     url = "https://400c3bfb-51ec-4397-aa27-28b8f8c30ca3.filesusr.com/archives/${version}.zip?dn=LinuxArcanists%202.zip";
     sha256 = "f587053f63a1f52cf993270d1b6408aa13dd295dc99ce96cf60547749d169da3";
   };
+
+  phases = [ "installPhase" ];
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/arcanists2
+    chmod +x $out/bin/arcanists2
+  '';
 }
