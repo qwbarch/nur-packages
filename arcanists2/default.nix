1ab9adb3d935b80471c3e264390802c956024820
qwbarch <42880469+qwbarch@users.noreply.github.com>
2023-02-18 18:51:49 -0500

    Use steam-run

--- arcanists2/default.nix
+++ arcanists2/default.nix
@@ -1,19 +1,33 @@
-{ stdenv }:
+{ pkgs, lib, stdenv, autoPatchelfHook, makeWrapper }:
 
 stdenv.mkDerivation rec {
   name = "arcanists2";
   version = "21254e_1fda44cb280f4146a4e471ac46995c81";
 
-  src = fetchUrl {
+  src = pkgs.fetchzip {
     url = "https://400c3bfb-51ec-4397-aa27-28b8f8c30ca3.filesusr.com/archives/${version}.zip?dn=LinuxArcanists%202.zip";
-    sha256 = "f587053f63a1f52cf993270d1b6408aa13dd295dc99ce96cf60547749d169da3";
+    sha256 = "b73shcHwpYxT1nHi3SGiGFa1LZXXkV6egV3tzbarQYQ=";
+    stripRoot = false;
   };
 
-  phases = [ "installPhase" ];
+  nativeBuildInputs = [ makeWrapper ];
+  buildInputs = with pkgs; [ steam-run-native ];
+
+  sourceRoot = ".";
+  dontConfigure = true;
+  dontBuild = true;
+
   installPhase = ''
+    mkdir -p $out/lib
     mkdir -p $out/bin
-    cp -a $src/. $out/bin
-    mv $out/bin/"Arcanists 2.x86_64" $out/bin/arcanists2
-    chmod +x $out/bin/arcanists2
+    cp -a $src/. $out/lib
+    chmod +x $out/lib/"Arcanists 2.x86_64"
+    makeWrapper ${pkgs.steam-run-native}/bin/steam-run $out/bin/arcanists2 --add-flags "$out/lib/'Arcanists 2.x86_64'"
   '';
+
+  meta = with lib; {
+    homepage = "https://www.arcanists2.com";
+    description = "Arcanists 2 is an online, turn-based multiplayer strategy game.";
+    platforms = platforms.linux;
+  };
 }
